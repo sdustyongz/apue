aaa5916d25859f8c4863d28a39f690a11849c5d8
xu.jie34 <xu.jie34@xcar.com.cn>
2022-11-30 10:47:45 +0800

    修改sigwait

--- 12/sigwait_test.c
+++ 12/sigwait_test.c
@@ -5,6 +5,9 @@
 #include <pthread.h>
 
  sigset_t mask;
+ int quitflag;
+ pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+ pthread_cond_t waitloc = PTHREAD_COND_INITIALIZER;
 
 void *  thr_fn(){
     int signo;
@@ -21,12 +24,17 @@ void *  thr_fn(){
                 printf("receive SIGINt\n");
                 break;
             case SIGQUIT:
+                pthread_mutex_lock(&lock);
+                quitflag = 1;
+                pthread_mutex_unlock(&lock);
+                pthread_cond_broadcast(&waitloc);
                 printf("receive SIGQUIT\n");
-                break;
+                return NULL;
             default:
                 break;
         }
     }
+    return NULL;
 }
 
 
@@ -37,13 +45,17 @@ int main(int argc,char * argv[]){
     sigaddset(&mask,SIGINT);
     sigaddset(&mask,SIGQUIT);
     sigset_t oldmask;
-    pthread_sigmask(SIG_BLOCK,&mask,&oldmask);
+    pthread_sigmask(SIG_SETMASK,&mask,&oldmask);
 
     if(pthread_create(&tid,NULL,thr_fn,(void *)NULL) != 0){
         perror("pthread create error");
         return 1;
     }
+    pthread_mutex_lock(&lock);
+    while(quitflag == 0)
+        pthread_cond_wait(&waitloc,&lock);
+    pthread_mutex_unlock(&lock);
 
-    pause();
+    sigprocmask(SIG_SETMASK,&oldmask,NULL);
     return 0;
 }
