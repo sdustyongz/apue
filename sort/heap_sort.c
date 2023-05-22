e58b5875cb3ac460e0731c2adbac038d38f3f347
syz <sdustyongz@126.com>
2023-05-22 15:49:46 -0700

    sort

--- sort/heap_sort.c
+++ sort/heap_sort.c
@@ -8,6 +8,7 @@
 #define  PARENT(i) (i)/2
 
 static int size = 0;
+static int  length = 0;
 
 struct element{
     int key;
@@ -40,11 +41,23 @@ void maxheapify(struct element * elms, int i){
 
 void build_max_heap(struct element * elms , int siz){
     size = siz;
+    length = siz;
     int i = floor(size/2);
     while(i >= 0){
         maxheapify(elms, i);
         i--;
     } 
+}
+
+void  heap_sort(struct element * els){
+    for(int i = size; i > 1 ; i--){
+        struct element  tmp = els[0];
+        els[0] = els[size -1];
+        els[size-1] = tmp;
+        size --;
+        maxheapify(els,0);
+    }
+    size = length;
 }
 
 
@@ -56,6 +69,10 @@ int main(int argc , char *argv[]){
     }
 
     build_max_heap(els, 5);
+    for(int i = 0;i < 5;i++)
+        printf("%d\n",els[i].key);
+    printf("----------------------------\n");
+    heap_sort(els);
     for(int i = 0;i < 5;i++)
         printf("%d\n",els[i].key);
 }
