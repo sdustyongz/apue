5e7b8417d626ac9c1ce323fd45d779c7b341242c
sdustyongz <sdustyongz@gmail.com>
2023-09-22 19:32:23 +0800

    b_tree

--- tree/b_tree.c
+++ tree/b_tree.c
@@ -21,14 +21,27 @@ BTNode * createThree(){
 }
 void insert_notfull_node(BTNode * x, int key);
 
+struct node_position{
+    BTNode * pr;
+    int index;
+};
+typedef struct node_position Np;
 
 
-BTNode * search(BTNode * node ,int key){
+Np * search(BTNode * node ,int key){
     int i = node->num-1;
     while(i >= 0 && key < node->value[i]){
         i--;
     }
-    if(i >=0 && key == node->value[i]) return node;
+    if(i >=0 && key == node->value[i]){
+        Np * np = malloc(sizeof(Np));
+        np->pr = node;
+        np->index = i;
+        return np;
+    } 
+    if(node->isLeaf){
+        return NULL;
+    }
     return search(node->ptr[i+1],key);
 }
 
@@ -116,7 +129,8 @@ void print(){
                 add(temp,(void*)bnode->ptr[j]);
             }
          }
-         add(temp,bnode->ptr[j]);
+         if(bnode->ptr[j] != NULL)
+            add(temp,bnode->ptr[j]);
          printf("    ");
        } 
        free(que);
@@ -124,6 +138,83 @@ void print(){
        temp = init_queue();
        printf("\n");
     }
+    free(que);
+    free(temp);
+}
+
+BTNode merge_brother_node(BTNode * parent,int leftIndex ,int rightIndex){
+    BTNode * lChild = parent->ptr[leftIndex];
+    BTNode * rChild = parent->ptr[rightIndex];
+    int i = leftIndex, j = lChild->num;
+    int value = parent->value[leftIndex];
+    //int ptr  = parent->ptr[leftIndex];
+    while(i < parent->num-1){
+        parent->value[i] = parent->value[i+1];
+        parent->ptr[i+1] = parent->ptr[i+2];
+    }
+    i = 0;
+    lChild[j++] = value;  
+    while(i < rchild->num){
+        lChild->value[j] = rChild->value[i];
+        lChild->ptr[j] = rChild->ptr[i];
+        i++;j++;
+    }
+    lChild->ptr[j] = rChild->ptr[lChild->num];
+}
+
+void delete_check_child(BTNode * node,int index){
+    int t = KEY_NUM/2+1;
+    int brothers[2];
+    if(index == 0) {
+        brothers[0] = 1;
+        brothers[1] = -1;
+    }else if(index == node->num + 1){
+        brothers[0] = node->num;
+        brothers[1] = -1;
+    }else{
+        brothers[0] = index;
+        brothers[1] = index+1; 
+    }
+    
+    if(node->prt[index]->num == t-1){
+        if(brothers[0] != -1 && node->ptr[brothers[0]]->num >=t){  
+
+        }else if(brothers[1] != -1 && node->ptr[brothers[1]]->num >=t) {
+
+        }else{
+
+            merge_brother_node(node,brothers);
+        }
+    }
+}
+
+void delete_node(int key){
+    BTNode * pr = root;
+    int i = pr->num - 1;
+    while(i >= 0 && pr->value[i] > key ){
+        i--;
+    }
+    if(i < 0){
+            
+    }else if(key == pr->value[i]){
+        if(pr->isLeaf){
+            delete_on_node(pr,i);
+        }else{
+            if(pr->ptr[i]->num > t-1){
+
+            }else if(pr->ptr[i+1]->num > t-1){
+
+            }else{
+
+            }
+        }
+
+    }else{
+        
+    }
+
+
+    
 }
 
 int main(int argc,char *argv[]){
@@ -146,6 +237,17 @@ int main(int argc,char *argv[]){
    print();
    printf("\n");
    insert_node(70);
-    print();
+   print();
+
+   Np * np =search(root,60);
+   if(np != NULL){
+    printf("%d\n",np->index);
+   }
+   np =search(root,100);
+   if(np != NULL){
+    printf("%d\n",np->index);
+   }else{
+    printf("cannotfonud\n");
+   }
 return 1;
 }
